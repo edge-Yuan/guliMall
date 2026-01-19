9069d4392200e9ebac4eac6828e0d869eca1654c
Edge Zhang <edge003@qq.com>
2026-01-19 14:59:24 +0800

    项目价结构初始创建

---
+++ gulimall-order/src/test/java/com/edgez/gulimall/order/GulimallOrderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgez.gulimall.order;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallOrderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
